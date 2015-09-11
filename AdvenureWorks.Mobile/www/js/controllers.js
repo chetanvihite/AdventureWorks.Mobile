6d12e990d9a80b5f83e41495c93cfb9d06fc0f68
chetanvihite <cvihite@gmail.com>
2015-09-11 20:49:56 +0530

    Added solution

---
+++ AdvenureWorks.Mobile/www/js/controllers.js
@@ -0,0 +1,27 @@
+angular.module('adventureWorksApp.controllers', [])
+
+.controller('DashCtrl', function ($scope) { })
+
+.controller('loginCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+})
+
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
