66c4f62f61601cae1e255aa4204289bf9d0fb7e7
chetanvihite <cvihite@gmail.com>
2015-09-16 18:37:47 +0530

    addded another project for services

---
+++ AdventureWorks.Mobile.Services/AdventureWorks.Mobile.Services/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace AdventureWorks.Mobile.Services
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+			config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "AdventureWorks",
+                routeTemplate: "api/AdventureWorks/Authenticate/{mobileNumber}/{password}",
+                defaults: new {
+                    mobileNumber = RouteParameter.Optional,
+                    password = RouteParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ AdventureWorks.Mobile.Services/AdventureWorks.Mobile.Services/Controllers/MobileServiceController.cs
@@ -0,0 +1,52 @@
+﻿
+
+
+using AdventureWorks.Mobile.Services._001_Domain;
+using AdventureWorks.Mobile.Services._002_Infra;
+using System.Web.Mvc;
+
+namespace AdventureWorks.Mobile.Services.Controllers
+{
+    [RoutePrefix("MobileService")]
+    public class MobileServiceController : Controller
+    {
+        // GET: MobileService
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string Test()
+        {
+            return "working fine";
+        }
+
+        [HttpGet]
+        [Route("Authenticate/{mobileNumber}/{password}")]
+        public JsonResult Authenticate(decimal mobileNumber, string password)
+        {
+            var result = new DbConnector().Authenticate(mobileNumber, password);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public UserProfile GetProfile(decimal userId)
+        {
+            return new DbConnector().GetProfile(userId);
+        }
+        [HttpGet]
+        public string Signup(decimal mobileNumber, string password, UserProfile profile)
+        {
+
+            return "success";
+        }
+        [HttpGet]
+        public OrderConfirmation SubmitOrder(decimal mobileNumber, Order order)
+        {
+
+
+            return new OrderConfirmation();
+        }
+    }
+}
